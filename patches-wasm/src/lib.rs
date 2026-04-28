5c43ae4b3e0840d5bc1a768460b27f98f453e5d2
Dominic Fox <dominic.fox@gmail.com@>
2026-04-28 18:58:02 +0100

    Remove wasm crates

--- patches-wasm/src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod loader;
-pub mod scanner;
-pub mod cache;
-
-pub use loader::{load_wasm_plugin, WasmModuleBuilder};
-pub use scanner::{scan_wasm_plugins, register_wasm_plugins};
-pub use cache::load_or_compile;
